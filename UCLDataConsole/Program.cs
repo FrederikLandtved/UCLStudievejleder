5bb098821d2dd77d013bd6c1470edd94e763018c
Frederik <Frederik@DESKTOP-NP3GB01>
2022-11-01 10:29:09 +0100

    Add project files.

---
+++ UCLDataConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+
+try
+{
+    string connectionString = "Server=tcp:ucldataserver.database.windows.net,1433;Initial Catalog=UCLDataPROD;Persist Security Info=False;User ID=azureuser;Password=Stefan$ebastianJacobMia;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+    SqlConnection connection = new SqlConnection(connectionString);
+
+    string sql = "SELECT * FROM [dbo].[Question]";
+
+    using (SqlCommand command = new SqlCommand(sql, connection))
+    {
+        connection.Open();
+        using (SqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                Question question = new Question
+                {
+                    Id = reader.GetInt32(0),
+                    QuestionString = reader.GetString(1),
+                    QuestionTypeId = reader.GetInt32(2)
+                };
+
+                Console.WriteLine(question.Id + question.QuestionString + question.QuestionTypeId);
+            }
+        }
+        connection.Close();
+    }
+
+}
+catch (Exception)
+{
+    Console.WriteLine("Something went wrong.");
+    throw;
+}
+
+public class Question
+{
+    public int Id { get; set; }
+    public int QuestionTypeId { get; set; }
+    public string QuestionString { get; set; }
+}
---
+++ UCLStudievejlederApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UCLStudievejlederApp.Models;
+
+namespace UCLStudievejlederApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
