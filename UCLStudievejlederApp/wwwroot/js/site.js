b3bfa9bc868079b88c7cc007a69f460b67309ae0
Sej1999 <74867161+FisterMedister@users.noreply.github.com>
2022-11-16 11:48:37 +0100

    added background blur

--- UCLStudievejlederApp/wwwroot/js/site.js
+++ UCLStudievejlederApp/wwwroot/js/site.js
@@ -5,9 +5,13 @@
 
 // OVERLAY
 function openNav() {
+    //document.getElementById("myNav").style.display = "block";
     document.getElementById("myNav").style.height = "60%";
+    document.getElementById("bg-blur").style.filter = "blur(8px)";
+    
 }
 
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
+    document.getElementById("bg-blur").style.filter = "blur(0px)";
 }
