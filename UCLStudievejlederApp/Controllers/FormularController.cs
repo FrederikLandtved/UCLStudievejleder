971f0a0a18870686d6e5acd8cdc9a5a8f64eb013
Frederik <Frederik@DESKTOP-NP3GB01>
2022-11-16 09:46:06 +0100

    Footer and Controller work

---
+++ UCLStudievejlederApp/Controllers/FormularController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UCLStudievejlederApp.Controllers
+{
+    public class FormularController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
