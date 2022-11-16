3982239149183c870110e2fa36973dba3aff904f
Sej1999 <74867161+FisterMedister@users.noreply.github.com>
2022-11-16 11:25:21 +0100

    Css stuff, yeah - Overlay

--- UCLStudievejlederApp/wwwroot/js/site.js
+++ UCLStudievejlederApp/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// OVERLAY
+function openNav() {
+    document.getElementById("myNav").style.height = "60%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
