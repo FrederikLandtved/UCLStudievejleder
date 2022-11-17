b4606455fceca9d1254e0c13b335667f4ed70a0d
Frederik <Frederik@DESKTOP-NP3GB01>
2022-11-17 11:37:56 +0100

    Register scaffold

--- UCLStudievejlederApp/Controllers/UserController.cs
+++ UCLStudievejlederApp/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UCLStudievejlederApp.Models.User;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +14,9 @@ namespace UCLStudievejlederApp.Controllers
         // GET: /<controller>/
         public IActionResult CreateUser()
         {
-            return View();
+            RegisterUserModel model = new RegisterUserModel();
+
+            return View(model);
         }
 
         public IActionResult EditUser()
