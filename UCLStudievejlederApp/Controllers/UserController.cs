3ce9b28a8d667dfbf0f5455d319a8ece7a043a38
Frederik <Frederik@DESKTOP-NP3GB01>
2022-11-17 19:02:23 +0100

    Moved Register User logic to our UserController

--- UCLStudievejlederApp/Controllers/UserController.cs
+++ UCLStudievejlederApp/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UCLStudievejlederApp.Models.User;
 
@@ -11,7 +12,39 @@ namespace UCLStudievejlederApp.Controllers
 {
     public class UserController : Controller
     {
-        // GET: /<controller>/
+        private SignInManager<IdentityUser> _signManager;
+        private UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager)
+        {
+            _userManager = userManager;
+            _signManager = signManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterUserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View();
+        }
+
         public IActionResult CreateUser()
         {
             RegisterUserModel model = new RegisterUserModel();
