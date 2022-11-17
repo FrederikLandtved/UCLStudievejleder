228dd57b519050ba9db4102f6a68c6f7e2e1f8b6
Frederik <Frederik@DESKTOP-NP3GB01>
2022-11-17 09:34:01 +0100

    Login page scaffolded

--- UCLStudievejlederApp/Controllers/FormularController.cs
+++ UCLStudievejlederApp/Controllers/FormularController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UCLStudievejlederApp.Controllers
 {
+    [Authorize]
     public class FormularController : Controller
     {
         public IActionResult Index()
