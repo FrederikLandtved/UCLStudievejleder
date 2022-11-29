24333e287a44325afcbd7fa54a1c1bffd04601b1
Frederik <Frederik@DESKTOP-NP3GB01>
2022-11-29 21:56:08 +0100

    Searching in Field of study on Register user page

---
+++ UCLStudievejlederApp/wwwroot/js/selection.js
@@ -0,0 +1,15 @@
+﻿function search() {
+    var inputText = document.getElementById('field-of-study-search').value;
+    var noResults = document.getElementById('no-results');
+
+    var results = document.getElementsByClassName('field-name');
+    var arr = Array.from(results);
+
+    arr.forEach(function (item) {
+        if (!item.innerHTML.toLowerCase().includes(inputText.toLowerCase())) {
+            item.closest("article").style.display = 'none';
+        } else {
+            item.closest("article").style.display = 'unset';
+        }
+    });
+}
