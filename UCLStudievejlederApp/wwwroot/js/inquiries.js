5909fac39a98cb43e9f4a0c47a6203485c885afa
Frederik <Frederik@DESKTOP-NP3GB01>
2022-11-30 11:24:50 +0100

    Namings

--- UCLStudievejlederApp/wwwroot/js/inquiries.js
+++ UCLStudievejlederApp/wwwroot/js/inquiries.js
@@ -13,28 +13,3 @@ function closeAll() {
         drop.classList.remove("show");
     })
 }
-
-//function dropDown() {
-//    document.getElementById("form-drop").classList.toggle("show");
-//}
-
-//function dropDown2() {
-//    document.getElementById("form-drop2").classList.toggle("show");
-//}
-
-//function dropDown3() {
-//    document.getElementById("form-drop3").classList.toggle("show");
-//}
-
-//window.onclick = function (event) {
-//    if (!event.target.matches('.form-title')) {
-//        var dropdowns = document.getElementsByClassName("form-drop-style");
-//        var i;
-//        for (i = 0; i < dropdowns.length; i++) {
-//            var openDropdown = dropdowns[i];
-//            if (openDropdown.classList.contains('show')) {
-//                openDropdown.classList.remove('show');
-//            }
-//        }
-//    }
-//}
