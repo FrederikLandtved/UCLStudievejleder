1e08bebc1808fb298786f49dd32350a60c8e464e
Frederik <Frederik@DESKTOP-NP3GB01>
2022-12-07 20:15:30 +0100

    Formular work in progress

---
+++ DatabaseAccess/AnswerOption/AnswerOptionDb.cs
@@ -0,0 +1,42 @@
+﻿using DatabaseAccess.Generic;
+using Microsoft.Data.SqlClient;
+using static DatabaseAccess.Generic.GenericSql;
+
+namespace DatabaseAccess.Question
+{
+    public class AnswerOptionDb
+    {
+        private readonly GenericSql _genericSql;
+        public AnswerOptionDb()
+        {
+            _genericSql = new GenericSql();
+        }
+
+        public List<AnswerOption> GetAnswerOptionsByQuestionId(int questionId)
+        {
+            List<AnswerOption> answerOptions = new List<AnswerOption>();
+            SqlDataReader reader = _genericSql.Select("SELECT * FROM [dbo].[AnswerOption] WHERE QuestionId = " + questionId);
+
+            while (reader.Read())
+            {
+                answerOptions.Add(new AnswerOption
+                {
+                    AnswerOptionId = reader.GetInt32(0),
+                    QuestionId = reader.GetInt32(1),
+                    AnswerOptionString = reader.GetString(2),
+                });
+            }
+
+            return answerOptions;
+        }
+
+
+        public class AnswerOption
+        {
+            public int AnswerOptionId { get; set; }
+            public int QuestionId { get; set; }
+            public string AnswerOptionString { get; set; }
+            public bool IsSelected { get; set; }
+        }
+    }
+}
--- UCLStudievejlederApp/Controllers/FormularController.cs
+++ UCLStudievejlederApp/Controllers/FormularController.cs
@@ -1,22 +1,30 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DatabaseAccess.Question;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UCLStudievejlederApp.Models.Formular;
 using UCLStudievejlederApp.Models.Generic;
+using static DatabaseAccess.Question.AnswerOptionDb;
+using static DatabaseAccess.Question.QuestionDb;
 
 namespace UCLStudievejlederApp.Controllers
 {
     [Authorize]
     public class FormularController : Controller
     {
+        private readonly QuestionDb _questionDb;
+        public FormularController()
+        {
+            _questionDb = new QuestionDb();
+        }
+
         public IActionResult Index()
         {
             FormularViewModel model = new FormularViewModel();
-
-            model.Questions.Add(new FormularAnswerModel { QuestionId = 1 });
+            model.Questions = _questionDb.GetAllQuestionsWithAnswers();
 
             for (int i = -12; i <= 0; i++)
             {
-                model.Questions[0].Options.Add(new UCLSelectModel { Id = i + 13, Name = DateTime.Now.AddMonths(i).ToString("MMMM yyyy") });
+                model.Questions[0].AnswerOptions.Add(new AnswerOption { QuestionId = 1, AnswerOptionString = DateTime.Now.AddMonths(i).ToString("MMMM yyyy") });
             }
 
             return View(model);
