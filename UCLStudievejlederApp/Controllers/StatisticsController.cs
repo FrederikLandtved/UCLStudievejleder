bd45ce00e6fb8cbb9c5c207caa4b942d47a43219
miadp88 <54668407+miadp88@users.noreply.github.com>
2023-01-04 11:25:44 +0100

    opbygning af tidsstatestik i cards. indsætning i veiwModel og @Model i index tilføjning af billeder/ikoner til cards(tidsforbrug)

--- UCLStudievejlederApp/Controllers/StatisticsController.cs
+++ UCLStudievejlederApp/Controllers/StatisticsController.cs
@@ -9,6 +9,7 @@ namespace UCLStudievejlederApp.Controllers
     {
         public IActionResult Index()
         {
+            
             StatisticViewModel statisticViewModel = new StatisticViewModel();
             FormularDb formularDb = new FormularDb();
             statisticViewModel.AllFormulars = formularDb.GetAllFormulars().Count();
@@ -26,7 +27,30 @@ namespace UCLStudievejlederApp.Controllers
 
             statisticViewModel.PhoneFormulars = phonePercent;
 
-            statisticViewModel.VirtuelleFormulars = formularDb.GetAmountOfAnswers(3, 2);
+
+            double amountOfVirtuelle = formularDb.GetAmountOfAnswers(3, 2);
+            double virtuellePercent = Math.Round((amountOfVirtuelle / statisticViewModel.AllFormulars) * 100, 1);
+
+
+            statisticViewModel.VirtuelleFormulars = virtuellePercent;
+
+            //Hvor mange procent af alle besvarelser har haft en samtaletid på 1-20 min.
+
+            double amountOfTimeUseOne = formularDb.GetAmountOfAnswers(4, 3);
+            double timeUseOne = Math.Round((amountOfTimeUseOne / statisticViewModel.AllFormulars) * 100, 1);
+
+            statisticViewModel.TimeUseOneFormulars = timeUseOne;
+
+            double amountOfTimeUseTwo = formularDb.GetAmountOfAnswers(5, 3);
+            double timeUseTwo = Math.Round((amountOfTimeUseTwo / statisticViewModel.AllFormulars) * 100, 1);
+
+            statisticViewModel.TimeUseTwoFormulars = timeUseTwo;
+
+            double amountOfTimeUseThree = formularDb.GetAmountOfAnswers(6, 3);
+            double timeUseThree = Math.Round((amountOfTimeUseThree / statisticViewModel.AllFormulars) * 100, 1);
+
+            statisticViewModel.TimeUseThreeFormulars = timeUseThree;
+
             return View(statisticViewModel);
         }
 
--- UCLStudievejlederApp/Models/StatisticViewModel.cs
+++ UCLStudievejlederApp/Models/StatisticViewModel.cs
@@ -5,7 +5,12 @@
         public double AllFormulars{ get; set; }
         public double PhoneFormulars{ get; set; }
 
-        public int VirtuelleFormulars { get; set; }
+        public double VirtuelleFormulars { get; set; }
         public double PersonalFormulars { get; set; }
+
+        public double TimeUseOneFormulars { get; set; }
+        public double TimeUseTwoFormulars { get; set; }
+        public double TimeUseThreeFormulars { get; set; }
+
     }
 }
