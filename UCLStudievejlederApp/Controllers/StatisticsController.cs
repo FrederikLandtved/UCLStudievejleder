64432378e4c4d2c157458914ed29fa57e177d8ef
miadp88 <54668407+miadp88@users.noreply.github.com>
2023-01-06 10:22:50 +0100

    indsætning af dynamisk data fra databasen i tabelen "Antal henvendelser per emne" i antal

--- UCLStudievejlederApp/Controllers/StatisticsController.cs
+++ UCLStudievejlederApp/Controllers/StatisticsController.cs
@@ -126,11 +126,23 @@ namespace UCLStudievejlederApp.Controllers
 
             statisticViewModel.AdminSubFormulars = adminSubPercent;
 
+            //Administrative forhold antal
+
+            int amountOfSubAdminNr = formularDb.GetAmountOfAnswers(69, 8);
+
+            statisticViewModel.AdminSubFormularsNr = amountOfSubAdminNr;
+        
+
             //Barsel i procent
             double amountOfSubBarsel = formularDb.GetAmountOfAnswers(70, 8);
             double barselSubPercent = Math.Round((amountOfSubBarsel / statisticViewModel.AllFormulars) * 100, 1);
 
             statisticViewModel.BarselSubFormulars = barselSubPercent;
+
+            //Barsel antal
+            int amountOfSubBarselNr = formularDb.GetAmountOfAnswers(70, 8);
+
+            statisticViewModel.BarselSubFormularsNr = amountOfSubBarselNr;
 
             //Eksamen i procent
             double amountOfSubEksamen = formularDb.GetAmountOfAnswers(71, 8);
@@ -138,11 +150,22 @@ namespace UCLStudievejlederApp.Controllers
 
             statisticViewModel.EksamenSubFormulars = eksamenSubPercent;
 
+            //Eksamen antal
+            int amountOfSubEksamenNr = formularDb.GetAmountOfAnswers(71, 8);
+
+            statisticViewModel.EksamenSubFormularsNr = amountOfSubEksamenNr;
+
+
             //Fastholde trivsel i procent
             double amountOfSubTrivsel = formularDb.GetAmountOfAnswers(72, 8);
             double trivselSubPercent = Math.Round((amountOfSubTrivsel / statisticViewModel.AllFormulars) * 100, 1);
 
             statisticViewModel.TrivselSubFormulars = trivselSubPercent;
+
+            //Fastholde trivsel antal
+            int amountOfSubTrivselNr = formularDb.GetAmountOfAnswers(72, 8);
+
+            statisticViewModel.TrivselSubFormularsNr = amountOfSubTrivselNr;
 
             //Internationale muligheder i procent
             double amountOfSubInternational = formularDb.GetAmountOfAnswers(73, 8);
@@ -150,11 +173,21 @@ namespace UCLStudievejlederApp.Controllers
 
             statisticViewModel.InternationalSubFormulars = internationalSubPercent;
 
+            //Internationale muligheder antal
+            int amountOfSubInternationalNr = formularDb.GetAmountOfAnswers(73, 8);
+
+            statisticViewModel.InternationalSubFormularsNr = amountOfSubInternationalNr;
+
             //Mistrivsel i procent
             double amountOfSubMistrivsel = formularDb.GetAmountOfAnswers(74, 8);
             double mistrivselSubPercent = Math.Round((amountOfSubMistrivsel / statisticViewModel.AllFormulars) * 100, 1);
 
             statisticViewModel.MistrivselSubFormulars = mistrivselSubPercent;
+
+            //Mistrivsel antal
+            int amountOfSubMistrivselNr = formularDb.GetAmountOfAnswers(74, 8);
+
+            statisticViewModel.MistrivselSubFormularsNr = amountOfSubMistrivselNr;
 
             //Optagelsesvejledning i procent
             double amountOfSubOptVej = formularDb.GetAmountOfAnswers(75, 8);
@@ -162,11 +195,21 @@ namespace UCLStudievejlederApp.Controllers
 
             statisticViewModel.OptVejSubFormulars = optVejSubPercent;
 
+            //Optagelsesvejledning antal
+            int amountOfSubOptVejNr = formularDb.GetAmountOfAnswers(75, 8);
+
+            statisticViewModel.OptVejSubFormularsNr = amountOfSubOptVejNr;
+
             //Ordensrelement i procent
             double amountOfSubOrdensrel = formularDb.GetAmountOfAnswers(76, 8);
             double ordensrelSubPercent = Math.Round((amountOfSubOrdensrel / statisticViewModel.AllFormulars) * 100, 1);
 
             statisticViewModel.OrdensrelSubFormulars = ordensrelSubPercent;
+
+            //Ordensrelement antal
+            int amountOfSubOrdensrelNr = formularDb.GetAmountOfAnswers(76, 8);
+
+            statisticViewModel.OrdensrelSubFormularsNr = amountOfSubOrdensrelNr;
 
             //Orlov i procent
             double amountOfSubOrlov = formularDb.GetAmountOfAnswers(77, 8);
@@ -174,11 +217,21 @@ namespace UCLStudievejlederApp.Controllers
 
             statisticViewModel.OrlovSubFormulars = orlovSubPercent;
 
+            //Orlov antal
+            int amountOfSubOrlovNr = formularDb.GetAmountOfAnswers(77, 8);
+
+            statisticViewModel.OrlovSubFormularsNr = amountOfSubOrlovNr;
+
             //Overflytning/genindskrivning i procent
             double amountOfSubOverflytGenind = formularDb.GetAmountOfAnswers(78, 8);
             double overflytGenindSubPercent = Math.Round((amountOfSubOverflytGenind / statisticViewModel.AllFormulars) * 100, 1);
 
             statisticViewModel.OverflytGenindSubFormulars = overflytGenindSubPercent;
+
+            //Overflytning/genindskrivning antal
+            int amountOfSubOverflytGenindNr = formularDb.GetAmountOfAnswers(78, 8);
+
+            statisticViewModel.OverflytGenindSubFormularsNr = amountOfSubOverflytGenindNr;
 
             //Personlige forhold i procent
             double amountOfSubPersonForh = formularDb.GetAmountOfAnswers(79, 8);
@@ -186,11 +239,23 @@ namespace UCLStudievejlederApp.Controllers
 
             statisticViewModel.PersonForhSubFormulars = personForhSubPercent;
 
+            //Personlige forhold antal
+
+            //Overflytning/genindskrivning antal
+            int amountOfSubPersonForhNr = formularDb.GetAmountOfAnswers(79, 8);
+
+            statisticViewModel.PersonForhSubFormularsNr = amountOfSubPersonForhNr;
+
             //Praktik i DK i procent
             double amountOfSubPraktik = formularDb.GetAmountOfAnswers(80, 8);
             double praktikSubPercent = Math.Round((amountOfSubPraktik / statisticViewModel.AllFormulars) * 100, 1);
 
             statisticViewModel.PraktikSubFormulars = praktikSubPercent;
+
+            //Praktik i DK antal
+            int amountOfSubPraktikNr = formularDb.GetAmountOfAnswers(80, 8);
+
+            statisticViewModel.PraktikSubFormularsNr = amountOfSubPraktikNr;
 
             //Ikke relevent henvendelse i procent
             double amountOfSubIkRel = formularDb.GetAmountOfAnswers(81, 8);
@@ -198,11 +263,21 @@ namespace UCLStudievejlederApp.Controllers
 
             statisticViewModel.IkRelSubFormulars = ikRelSubPercent;
 
+            //Ikke relevent henvendelse antal
+            int amountOfSubIkRelNr = formularDb.GetAmountOfAnswers(81, 8);
+
+            statisticViewModel.IkRelSubFormularsNr = amountOfSubIkRelNr;
+
             //SPS i procent
             double amountOfSubSPS = formularDb.GetAmountOfAnswers(82, 8);
             double sPSSubPercent = Math.Round((amountOfSubSPS / statisticViewModel.AllFormulars) * 100, 1);
 
             statisticViewModel.SPSSubFormulars = sPSSubPercent;
+
+            //SPS antal
+            int amountOfSubSPSNr = formularDb.GetAmountOfAnswers(82, 8);
+
+            statisticViewModel.SPSSubFormularsNr = amountOfSubSPSNr;
 
             //Studieophør i procent
             double amountOfSubStudOph = formularDb.GetAmountOfAnswers(83, 8);
@@ -210,11 +285,21 @@ namespace UCLStudievejlederApp.Controllers
 
             statisticViewModel.StudOphSubFormulars = studOphSubPercent;
 
+            //Studieophør antal
+            int amountOfSubStudOphNr = formularDb.GetAmountOfAnswers(83, 8);
+
+            statisticViewModel.StudOphSubFormularsNr = amountOfSubStudOphNr;
+
             //Studieplanlægning i procent
             double amountOfSubStdPlan = formularDb.GetAmountOfAnswers(84, 8);
             double stdPlanSubPercent = Math.Round((amountOfSubStdPlan / statisticViewModel.AllFormulars) * 100, 1);
 
             statisticViewModel.StdPlanSubFormulars =stdPlanSubPercent;
+
+            //Studieplanlægning antal
+            int amountOfSubStdPlanNr = formularDb.GetAmountOfAnswers(84, 8);
+
+            statisticViewModel.StdPlanSubFormularsNr = amountOfSubStdPlanNr;
 
             //Studietvivl/studievalg i procent
             double amountOfSubStdTvivl = formularDb.GetAmountOfAnswers(85, 8);
@@ -222,11 +307,21 @@ namespace UCLStudievejlederApp.Controllers
 
             statisticViewModel.StdTvivlSubFormulars = stdTvivlSubPercent;
 
+            //Studietvivl/studievalg antal
+            int amountOfSubStdTvivlNr = formularDb.GetAmountOfAnswers(85, 8);
+
+            statisticViewModel.StdTvivlSubFormularsNr = amountOfSubStdTvivlNr;
+
             //Studieudfordringer i procent
             double amountOfSubStdUdf = formularDb.GetAmountOfAnswers(86, 8);
             double stdUdfSubPercent = Math.Round((amountOfSubStdUdf / statisticViewModel.AllFormulars) * 100, 1);
 
             statisticViewModel.StdUdfSubFormulars = stdUdfSubPercent;
+
+            //Studieudfordringer antal
+            int amountOfSubStdUdfNr = formularDb.GetAmountOfAnswers(86, 8);
+
+            statisticViewModel.StdUdfSubFormularsNr = amountOfSubStdUdfNr;
 
             //Sygdom(egne) i procent
             double amountOfSubSygEgne = formularDb.GetAmountOfAnswers(87, 8);
@@ -234,17 +329,32 @@ namespace UCLStudievejlederApp.Controllers
 
             statisticViewModel.SygEgneSubFormulars = sygEgneSubPercent;
 
+            //Sygdom(egne) antal
+            int amountOfSubSygEgneNr = formularDb.GetAmountOfAnswers(87, 8);
+
+            statisticViewModel.SygEgneSubFormularsNr = amountOfSubSygEgneNr;
+
             //Undervisningen (Samarbejdsvanskligheder) i procent
             double amountOfSubUnvSamaVan = formularDb.GetAmountOfAnswers(88, 8);
             double unvSamaVanSubPercent = Math.Round((amountOfSubUnvSamaVan / statisticViewModel.AllFormulars) * 100, 1);
 
             statisticViewModel.UnvSamaVanSubFormulars = unvSamaVanSubPercent;
 
+            //Undervisningen (Samarbejdsvanskligheder) antal
+            int amountOfSubUnvSamaVanNr = formularDb.GetAmountOfAnswers(88, 8);
+
+            statisticViewModel.UnvSamaVanSubFormularsNr = amountOfSubUnvSamaVanNr;
+
             //Økonomi i procent
             double amountOfSubOkonomi = formularDb.GetAmountOfAnswers(89, 8);
             double okonomiSubPercent = Math.Round((amountOfSubOkonomi / statisticViewModel.AllFormulars) * 100, 1);
 
             statisticViewModel.OkonomiSubFormulars = okonomiSubPercent;
+
+            //Økonomi antal
+            int amountOfSubOkonomiNr = formularDb.GetAmountOfAnswers(89, 8);
+
+            statisticViewModel.OkonomiSubFormularsNr = amountOfSubOkonomiNr;
 
             return View(statisticViewModel);
         }
--- UCLStudievejlederApp/Models/StatisticViewModel.cs
+++ UCLStudievejlederApp/Models/StatisticViewModel.cs
@@ -31,7 +31,7 @@
         public double TimeUseThreeCountFormulars { get; set; }
 
 
-        //Procent svarede emne
+        //Formulare per emne i procent
 
         public double AdminSubFormulars  { get; set; }
 
@@ -67,5 +67,47 @@
 
         public double OkonomiSubFormulars { get; set; }
 
+        //Formulare per emne i procent
+
+
+
+
+        //Formulare per emne i antal
+
+        public double AdminSubFormularsNr { get; set; }
+
+        public double BarselSubFormularsNr { get; set; }
+        public double EksamenSubFormularsNr { get; set; }
+
+        public double TrivselSubFormularsNr { get; set; }
+
+        public double InternationalSubFormularsNr { get; set; }
+
+        public double MistrivselSubFormularsNr { get; set; }
+
+        public double OptVejSubFormularsNr { get; set; }
+
+        public double OrdensrelSubFormularsNr { get; set; }
+
+        public double OrlovSubFormularsNr { get; set; }
+
+        public double OverflytGenindSubFormularsNr { get; set; }
+
+        public double PersonForhSubFormularsNr { get; set; }
+        public double PraktikSubFormularsNr { get; set; }
+        public double IkRelSubFormularsNr { get; set; }
+
+        public double SPSSubFormularsNr { get; set; }
+        public double StudOphSubFormularsNr { get; set; }
+        public double StdPlanSubFormularsNr { get; set; }
+        public double StdTvivlSubFormularsNr { get; set; }
+
+        public double StdUdfSubFormularsNr { get; set; }
+        public double SygEgneSubFormularsNr { get; set; }
+        public double UnvSamaVanSubFormularsNr { get; set; }
+
+        public double OkonomiSubFormularsNr { get; set; }
+
     }
 }
+
